1f6d42f184615e299e4215484d9fbfdb4b95b166
Juliana Jaime <27710492+peppermint-juli@users.noreply.github.com>
2025-12-03 13:58:22 -0500

    feat: add orientation page component with NewPayment import

---
+++ lab5/webapp/app/orientation/page.tsx
@@ -0,0 +1,12 @@
+import { NewPayment } from 'components/contents/newPayment';
+import { OrientationInfo } from 'components/contents/orientationInfo';
+
+
+// TODO: imp loading animation here
+export default async function InfoPage() {
+
+  // data is now fully typed as Item[]
+  return (
+    <OrientationInfo />
+  );
+};
